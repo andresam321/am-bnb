0b878b56067130e6a4f7aa056ae7b11358fc913a
Andres Mercado <andresmercado96@gmail.com>
2024-02-22 15:29:10 -0800

    Add User authentication Express Middleware

--- backend/db/migrations/20240222212335-create-user.js
+++ backend/db/migrations/20240222212335-create-user.js
@@ -44,6 +44,6 @@ module.exports = {
   },
   async down(queryInterface, Sequelize) {
     options.tableName = "Users";
-    return queryInterface.dropTable(options);
+    await queryInterface.dropTable(options);
   }
 };
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,43 @@
 const router = require('express').Router();
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+const { restoreUser } = require('../../utils/auth.js');
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
   });
+
+  // GET /api/set-token-cookie
+
+router.get('/set-token-cookie', async (_req, res) => {
+  const user = await User.findOne({
+    where: {
+      username: 'Demo-lition'
+    }
+  });
+  setTokenCookie(res, user);
+  return res.json({ user: user });
+});
+
+
+
+
+router.use(restoreUser);
+
+router.get(
+  '/restore-user',
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+
+const { requireAuth } = require('../../utils/auth.js');
+router.get(
+  '/require-auth',
+  requireAuth,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
 
 module.exports = router;
