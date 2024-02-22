2a8949415e45f7cb4690b977290ec0292640b927
Andres Mercado <andresmercado96@gmail.com>
2024-02-22 14:34:46 -0800

    Add Users model scope for user security

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -46,7 +46,12 @@ module.exports = (sequelize, DataTypes) => {
       }
     }, {
       sequelize,
-      modelName: 'User'
+      modelName: 'User',
+      defaultScope:{
+        attributes:{
+        exclude:["hashedPassword", "email","createdAt","updatedAt"]
+        }
+      }
     }
   
   );
