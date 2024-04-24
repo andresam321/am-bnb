7762b50ddcb598edfce3c415e9e4655c37f942e1
Andres Mercado <andresmercado96@gmail.com>
2024-04-23 20:47:36 -0700

    added maps and review modal is working

--- backend/psql-setup-script.js
+++
@@ -1,7 +0,0 @@
-const { sequelize } = require('./db/models');
-
-sequelize.showAllSchemas({ logging: false }).then(async (data) => {
-  if (!data.includes(process.env.SCHEMA)) {
-    await sequelize.createSchema(process.env.SCHEMA);
-  }
-});
